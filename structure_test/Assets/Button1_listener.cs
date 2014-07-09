80b3b4c6a944f41fe2b09b4f59c17a0cb84b4c93
sp9103 <sp9103@naver.com>
2014-07-09 23:51:27 +0900

    -전체 프로젝트 삭제하고 새로 올림 ㄷㄷ

---
+++ structure_test/Assets/Button1_listener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button1_listener : MonoBehaviour {
+
+	public GameObject Target_light;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start Complete");
+		Target_light = GameObject.Find ("Camera Light");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+				//터치 ray tracing
+				Vector3 ray = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+				
+				Debug.Log ("Click!!");
+				Target_light.light.color = Color.blue;
+			}
+		} else {
+			if (Input.GetMouseButtonDown (0)) {
+				//터치 ray tracing
+				Vector3 ray = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				
+				Debug.Log ("Click!!");
+				Target_light.light.color = Color.blue;
+			}
+		}
+	}
+}
---
+++ structure_test/Assets/Button3_listener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button3_listener : MonoBehaviour {
+
+	public GameObject Target_light;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start Complete");
+		Target_light = GameObject.Find ("Camera Light");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetTouch (0).phase == TouchPhase.Began) {
+			Debug.Log ("Click!!");
+			Target_light.light.color = Color.white;
+		}
+	}
+}

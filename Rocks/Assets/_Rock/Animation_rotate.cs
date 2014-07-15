53ea05a1d4d0fe90084bfcebd2c4b8ef72099d23
정 락준 <jrj325@hanmir.com>
2014-07-15 16:08:21 +0900

    우헤

---
+++ Rocks/Assets/_Rock/Animation_rotate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Rock");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	IEnumerator Rock(){
+		float radius = 10.0f;
+		float x=radius,z=0;
+		int wise = 0;
+		float rad = 0;
+		while(true){
+			yield return new WaitForSeconds(0.01f);
+//			this.transform.position= new Vector3(0.0f,0.0f,0.0f);
+			this.transform.position= new Vector3(x,0.0f,z);
+			this.transform.eulerAngles = new Vector3(0.0f,-rad/Mathf.PI*180.0f,0.0f);
+			rad += 0.1f;
+			x = Mathf.Cos (rad) * radius;
+			z = Mathf.Sin (rad) * radius;
+			Debug.Log(x.ToString()+","+z.ToString());
+		}
+	}
+}
---
+++ Rocks/Assets/_Rock/Straight_man.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Straight_man : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Straight_Rock");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	const float speedz = 0.005f;
+	float dx = 0.0f;
+	float dz = speedz;
+	float x = 0.0f;
+	float z = 1.0f;
+	IEnumerator Straight_Rock(){
+		while(true){
+			yield return new WaitForSeconds(0.001f);
+			x += dx;
+			z += dz;
+			this.transform.position= new Vector3(x,0.01f,z);
+			float angle = Mathf.Atan2 (dx,dz) * Mathf.Rad2Deg;
+			this.transform.eulerAngles = new Vector3(0.0f,angle,0.0f);
+		}
+	}
+	float rad = 0.0f;
+	void OnCollisionEnter(Collision collision) {
+//		Vector3 from = collision.transform.position;
+//		Vector3 to = this.transform.position;
+		StopCoroutine("Straight_Rock");
+		x -= dx*2;
+		z -= dz*2;
+		this.transform.position= new Vector3(x,0.01f,z);
+		float angle = Mathf.Atan2 (dx,dz) * Mathf.Rad2Deg;
+		this.transform.eulerAngles = new Vector3(0.0f,angle,0.0f);
+
+		rad += (Mathf.PI/ 36.0f);
+		dx = Mathf.Sin (rad) * speedz;
+		dz = Mathf.Cos (rad) * speedz;
+		Debug.Log("Collision "+rad.ToString());
+		StartCoroutine("Straight_Rock");
+	}
+}

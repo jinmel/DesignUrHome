4ea716bedf4cf5ea2029ce11e844d4cd6af6364d
Moon Sung phill <sp9103@naver.com>
2014-07-22 19:17:21 +0900

    -일부 버튼 수정. -structure 위치 보정
    * 캐릭터 생성 & 움직임 표현 구현 시작

--- structure_test/Assets/My_script/Button1_listener.cs
+++ structure_test/Assets/My_script/Button1_listener.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class Button1_listener : MonoBehaviour {
 
 	public GameObject _light;
+	private int counter;
 	// Use this for initialization
 	void Start () {
+		counter = 0;
 	}
 	
 	// Update is called once per frame
@@ -14,7 +16,19 @@ public class Button1_listener : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		_light.light.color = Color.blue;
+		counter = (counter + 1) % 3;
+
+		switch (counter) {
+		case 0:
+			_light.light.color = Color.white;
+			break;
+		case 1:
+			_light.light.color = Color.blue;
+			break;
+		case 2:
+			_light.light.color = Color.green;
+			break;
+				}
 	}
 	
 }
--- structure_test/Assets/My_script/Button3_listener.cs
+++ structure_test/Assets/My_script/Button3_listener.cs
@@ -2,18 +2,43 @@
 using System.Collections;
 
 public class Button3_listener : MonoBehaviour {
+	//view mode
+	//////////////////////////////////////////////
+	// mode 0 : default
+	// mode 1 : bird-eyes view
+	// mode 2 : target following view mode
+	//////////////////////////////////////////////
 
-	public GameObject _light;
+	private int mode_checker;
+	public GameObject main_model;
+
 	// Use this for initialization
 	void Start () {
+		mode_checker = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 	
 	void OnMouseDown() {
-		_light.light.color = Color.white;
+		mode_checker = (mode_checker + 1) % 3;
+
+		switch (mode_checker) {
+		case 0:
+			//destroy all human model
+
+			break;
+		case 1:
+			//Create human model & not change camera
+			//Create main model & attach model controller
+
+			break;
+		case 2:
+			//disable ARcamera & change camera view
+			//when changeing view, camera pos & rotation => ARcamera pos to main model
+
+			break;
+				}
 	}
 }

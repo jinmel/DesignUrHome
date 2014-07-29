29c7722635b5186eaec443ab52e30fb8dd234b7c
정 락준 <jrj325@hanmir.com>
2014-07-29 15:52:36 +0900

    버튼2 가구 오브젝트를 삽입하는 기능 추가함.

--- structure_test/Assets/My_script/Button2_listener.cs
+++ structure_test/Assets/My_script/Button2_listener.cs
@@ -17,16 +17,5 @@ public class Button2_listener : MonoBehaviour {
 
 	void OnMouseDown() {
 		Singleton.GetInstance().Mode = 2;
-
-		Vector3 Pos = GameObject.Find ("ARCamera").transform.position;
-		Pos.y -= 130;
-
-		int x = Random.Range (-50, 50);
-		int z = Random.Range (-50, 50);
-
-		Pos.x -= x;
-		Pos.z -= z;
-
-		Instantiate (DropIt, Pos, DropIt.transform.rotation);
 	}
 }

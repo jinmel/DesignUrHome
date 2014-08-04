b2ced52ef73b307825ee5da6d70fc4457e722bf6
Moon Sung phill <sp9103@naver.com>
2014-08-04 20:24:00 +0900

    - 캐릭터 회전 추가
    * 캐릭터 애니메이션 & 1인칭 모드시 움직임 재 정의

--- src/Assets/Scripts/Pad_Controller.cs
+++ src/Assets/Scripts/Pad_Controller.cs
@@ -10,16 +10,16 @@ public class Pad_Controller : MonoBehaviour
 		public Camera main_cam;
 		private float _time;
 		private bool touch_check;
-
+	
 		//Move Key pad
 		private Vector3 Start_point;		//Initial Start point
 		private float Button_Dist;
 		private float Model_acceleration;
-
+	
 		//Local position
 		private Vector3 Local_start;
 		private Vector3 Local_target;
-
+	
 		// Use this for initialization
 		void Start ()
 		{
@@ -27,7 +27,7 @@ public class Pad_Controller : MonoBehaviour
 				touch_check = false;
 				Button_Dist = 0.7f;
 				Model_acceleration = 0.5f;
-
+		
 				Color _t_gray = new Color (0.2f, 0.2f, 0.2f, 1.0f);
 				Move_board.renderer.material.SetColor ("_Color", _t_gray);
 		}
@@ -35,7 +35,7 @@ public class Pad_Controller : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-	
+		
 				if (Time.timeSinceLevelLoad - _time > 0.5) {
 						if (Input.GetMouseButtonDown (0)) {
 								touch_check = true;
@@ -50,14 +50,14 @@ public class Pad_Controller : MonoBehaviour
 								Move_board.SetActive (false);
 								touch_check = false;
 						}
-						
+			
 						if (touch_check == true) {
 								//버튼 따라다니게 하기
 								Vector3 target_pos = Calculate_button_pos (10);
 								Move_key.transform.position = target_pos;
 								Local_target = Move_key.transform.localPosition;
 								float dist = Vector3.Distance (Local_target, Local_start);
-								
+				
 								//일정 범위 이상 안벗어 나게...
 								if (dist > Button_Dist) {
 										Vector3 Key_dir = Vector3.Normalize (Local_target - Local_start);
@@ -65,14 +65,26 @@ public class Pad_Controller : MonoBehaviour
 										Vector3 dst_pos = Local_start + Key_dir;
 										Move_key.transform.localPosition = dst_pos;
 								}
-
-								//Move Charecter
+				
+								//Rotate Charecter
+								//axis-z is main direction
+								float t_angle;
+								Vector3 z_axis = new Vector3 (0, 0, 1);
 								Vector3 Char_dir = GetModel_Direction ();
+								Vector3 t_cross_result = Vector3.Cross (z_axis, Char_dir);
+								t_angle = Vector3.Angle (z_axis, Char_dir);
+				
+								if (t_cross_result.y < 0)
+										t_angle *= -1.0f;
+				
+								Charecter.transform.rotation = Quaternion.AngleAxis (t_angle, Vector3.up);
+				
+								//Move Charecter
 								Charecter.transform.position += Char_dir;
 						}
 				}
 		}
-
+	
 		Vector3 Calculate_button_pos (int dist)
 		{
 				Vector3 screen_pos = Input.mousePosition;
@@ -82,38 +94,38 @@ public class Pad_Controller : MonoBehaviour
 				float cam_ray_dist = Mathf.Sqrt (Cam_ray_vec.magnitude);				//distance - cam, ray_origin
 				float sub_dist = dist - cam_ray_dist;
 				Vector3 dst_pos = touch_ray.GetPoint (sub_dist);
-
+		
 				return dst_pos;
 		}
-
+	
 		//Get Model move direction
 		//return normalized vector.
 		private Vector3 GetModel_Direction ()
 		{
-				Vector3 Start_floor_pos = GetFloor_pos (Start_point);
+				Vector3 Start_floor_pos = GetFloor_pos (Move_board.transform.position);
 				Vector3 GameKey_floor_pos = GetFloor_pos (Move_key.transform.position);
-
+		
 				Vector3 Dir_vec = GameKey_floor_pos - Start_floor_pos;
 				return Dir_vec.normalized;
 		}
-
+	
 		//Gamepad_pos + Ray_vec*t = floor_pos.
 		//This fuction calculate t.
 		private float GetScreentoFloor_Const (Vector3 p)
 		{
 				Vector3 t_Ray = p - main_cam.transform.position;
-			
+		
 				return p.y / (-t_Ray.y);
 		}
-
+	
 		private Vector3 GetFloor_pos (Vector3 p)
 		{
 				Vector3 t_Ray = p - main_cam.transform.position;
 				float t_const = GetScreentoFloor_Const (p);	
-
+		
 				return p + t_const * t_Ray;
 		}
-
+	
 		void OnGUI ()
 		{
 				Vector3 Cam_pos = main_cam.transform.position;

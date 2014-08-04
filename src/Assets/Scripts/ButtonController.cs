c340a6070a8fab464030f4b964bd5419c8346e98
Moon Sung phill <sp9103@naver.com>
2014-08-04 19:48:20 +0900

    - 캐릭터 모델 포함을 제외한 이전 모든 커밋 머지 완료
    * 추가작업 캐릭터 모델 추가

--- src/Assets/Scripts/ButtonController.cs
+++ src/Assets/Scripts/ButtonController.cs
@@ -6,19 +6,20 @@ public class ButtonController : MonoBehaviour
 	
 		//MainScene Object
 		public Camera ARCamera;						// vuforia camera
-		public GameObject GamePad;					// GamePad. in button3
 		public Camera CAM;							// external Camera (using button3)
 		public GameObject Light;					// main Light in Camera
+		public GameObject GamePad;					// GamePad. in button3
 		public GameObject ImageTarget;				// ImageTarget in vuforia
 		public GameObject Character;				// Charector model
-
+	
 		//...
-		private Object tLight;						// Button3 - mode3. 1인칭 시점에서 사용
 		private GameObject tFloor;
+		private GameObject tStructure;				// Button3 - mode3. 1인칭 시점에서 구조
+		private Object tLight;						// Button3 - mode3. 1인칭 시점에서 사용
 		private bool model_render_check;			// Button4 - 
 		private int mode_checker;
 		private int counter;
-		private GameObject tStructure;				// Button3 - mode3. 1인칭 시점에서 구조
+		private string prev_Target_name;
 
 		// ... 
 		public GUIStyle container_style;
@@ -32,6 +33,7 @@ public class ButtonController : MonoBehaviour
 		void Start ()
 		{
 				mode_checker = 0;
+				prev_Target_name = null;
 		}
 	
 		// Update is called once per frame
@@ -70,89 +72,83 @@ public class ButtonController : MonoBehaviour
 						// mode 1 : bird-eyes view
 						// mode 2 : target following view mode
 						//////////////////////////////////////////////
-
+			
 						ContentManager.getInstance ().Mode = 3;
 						mode_checker = (mode_checker + 1) % 3;
-
+			
 						GameObject tImageTarget;					//SceneManger -> ImageTarget find
 						GameObject target_structure;				//Imagetarget first child component.(first son is structure)
-
+						string t_name = ContentManager.getInstance ().imageTargetName;
+			
+						if (t_name != null && !t_name.Equals (prev_Target_name)) {
+								Mode3_Initialize ();
+								mode_checker = 1;
+						}
+			
 						switch (mode_checker) {
 						case 0:
-       
 								//destroy all human model
-								//TO-DO
-
-    						    //Destroy temp Light
-								DestroyObject (tLight);
-								CAM.gameObject.SetActive (false);
-								
-								//Destroy temp Structure
-								DestroyObject (tStructure);
-
-								//kill character model
-								Character.SetActive (false);
-
-						        //Set Active Cam
-								ARCamera.gameObject.SetActive (true);
-								ImageTarget.SetActive (true);
-
-								//killllllll Gamepad
-								GamePad.SetActive (false);
-
+								Mode3_Initialize ();
+				
 								break;
 						case 1:
 								//Tracking nothing - action nothing
-								if (ContentManager.getInstance ().imageTargetName == null) {
+								if (t_name == null) {
 										mode_checker = 0;
 										break;
 								}
-
-    						    //Create human model & not change camera
-        						//Create main model & attach model controller
+				
+								//Create human model & not change camera
+								//Create main model & attach model controller
 								GamePad.SetActive (true);
-								
-								tImageTarget = GameObject.Find (ContentManager.getInstance ().imageTargetName);
-								
+				
+								tImageTarget = GameObject.Find (t_name);
+				
 								Character.transform.position = tImageTarget.transform.position;
-								Character.SetActive (true);
 								Character.transform.parent = tImageTarget.transform.GetChild (0).gameObject.transform;
+								Character.SetActive (true);
 								break;
 						case 2:
 								//disable ARcamera & change camera view
 								//when changeing view, camera pos & rotation => ARcamera pos to main model
 								//AR_Camera.gameObject.SetActive(false);
 								GamePad.SetActive (false);
-
-        						//Main Light Copy
+				
+								//Main Light Copy
 								tLight = Instantiate (Light, Light.transform.position, Light.transform.rotation);
-
+				
 								CAM.transform.position = ARCamera.transform.position;
 								CAM.transform.rotation = ARCamera.transform.rotation;
 								CAM.gameObject.SetActive (true);
-								
+				
+								//Camera moving start;
+								CAM.gameObject.transform.GetComponent<Set_CamPos> ().Cam_posSet ();
+				
 								//Create Structure
-								tImageTarget = GameObject.Find (ContentManager.getInstance ().imageTargetName);
+								tImageTarget = GameObject.Find (t_name);
 								target_structure = tImageTarget.transform.GetChild (0).gameObject;
 								tStructure = (GameObject)Instantiate (target_structure, target_structure.transform.position, target_structure.transform.rotation);
-
+				
 								//Create Floor
 								//...				
-
+				
 								//Set structure scale
 								Vector3 t_scale = new Vector3 (target_structure.transform.localScale.x * tImageTarget.transform.localScale.x
-				                				              , target_structure.transform.localScale.y * tImageTarget.transform.localScale.y
-				                              					, target_structure.transform.localScale.z * tImageTarget.transform.localScale.z);
+				                               , target_structure.transform.localScale.y * tImageTarget.transform.localScale.y
+				                               , target_structure.transform.localScale.z * tImageTarget.transform.localScale.z);
 								tStructure.transform.localScale = t_scale;
-
+				
 								//ARCamera shut down
 								ARCamera.gameObject.SetActive (false);
 								ImageTarget.SetActive (false);
-
-								
-
+				
+				
+				
 								break;
 						}
+			
+						//Store prev target name
+						prev_Target_name = t_name;
 				}
 
 
@@ -168,5 +164,25 @@ public class ButtonController : MonoBehaviour
 						}
 				}
 				GUI.Label (new Rect (200, 5, 30, 30), ContentManager.getInstance ().imageTargetName, tDebug);
+		}
+
+		private void Mode3_Initialize ()
+		{
+				//Destroy temp Light
+				DestroyObject (tLight);
+				CAM.gameObject.SetActive (false);
+		
+				//Destroy temp Structure
+				DestroyObject (tStructure);
+		
+				//kill character model
+				Character.SetActive (false);
+		
+				//Set Active Cam
+				ARCamera.gameObject.SetActive (true);
+				ImageTarget.SetActive (true);
+		
+				//killllllll Gamepad
+				GamePad.SetActive (false);
 		}
 }
---
+++ src/Assets/Scripts/Set_CamPos.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Set_CamPos : MonoBehaviour
+{
+
+		private static bool Start_set = false;
+		private float t_time;
+		private const float Moving_time = 2.0f;
+		private Vector3 src_pos;
+	private Vector3 src_rot;
+		private Vector3 dst_pos = new Vector3 (0.0f, 2.0f, 0.0f);		//local coordinate
+		private Vector3 dst_rot = new Vector3 (30.0f, 0.0f, 0.0f);
+		private Vector3 dir_vec;
+		private Vector3 dir_rot;
+		// Use this for initialization
+		void Start ()
+		{
+				t_time = 0.0f;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Start_set == true) {
+						if (t_time == 0.0f) {
+								t_time = Time.timeSinceLevelLoad;
+
+								src_pos = this.transform.localPosition;
+				src_rot = this.transform.localRotation.eulerAngles;
+								
+								dir_vec = (dst_pos - this.transform.localPosition) / Moving_time;
+								dir_rot = (dst_rot - this.transform.localRotation.eulerAngles) / Moving_time;
+						} else {
+								float t = Time.timeSinceLevelLoad - t_time;
+								
+								if (t > Moving_time) {
+										t_time = 0.0f;
+										Start_set = false;
+
+										this.transform.localPosition = dst_pos;
+										this.transform.localRotation = Quaternion.Euler (dst_rot);
+								} else {
+					this.transform.localRotation = Quaternion.Euler(src_rot + t * dir_rot);
+										this.transform.localPosition = src_pos + t * dir_vec;
+								}
+						}
+				}
+		}
+
+		public void Cam_posSet ()
+		{
+				Start_set = true;
+		}
+}

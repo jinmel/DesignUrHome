c7cce79b9d1b672245444f7752a1b55ec02d4594
Jin Suk Park <totorovirus@gmail.com>
2014-08-06 17:15:05 +0900

    Prefeb-> Prefab / Inventory Controller Event handled manually

--- src/Assets/Scripts/InventoryController.cs
+++ src/Assets/Scripts/InventoryController.cs
@@ -1,11 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-enum ItemNumber
-{
-    None = 0,
-    Bed}
-;
+using System.Collections.Generic;
 
 public class InventoryController : MonoBehaviour
 {
@@ -13,53 +8,53 @@ public class InventoryController : MonoBehaviour
     public GUIStyle inventoryBoxStyle;
 
     //items for Inventory
-    public Texture2D bedImage;
-    public GameObject bedModel;
+    public Texture2D[] images;
+    public GameObject[] models;
 
-    //status variables
-    private ItemNumber curItem;
-        
-        
+    //privat variables
+    private GameObject curModel;
+    private List<GameObject> objectInstanceList; // keep the list of instance of inventory items created by user. 
+    private bool inventoryItemSelected;
+
+
+  
     // Use this for initialization
     void Start()
     {
-        Debug.Log("created");
-        Debug.Log("width:" + Screen.width.ToString() + " height:" + (string)Screen.height.ToString());
-        //instantiate bed object
-        Instantiate(bedModel);
+        objectInstanceList = new List<GameObject>();
+        inventoryItemSelected = false;
+
     }
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousepos = Input.mousePosition;
 
-        if (Input.GetMouseButton(0))
-        {
-            switch (curItem)
-            {
-            //item 1 - bed
-                case ItemNumber.Bed:
-                    bedModel.transform.position = mousepos;
-                    bedModel.SetActive(true);
-                    break;
-                default:
-                    break;
-            }
-
-        } else
-        {
-            switch (curItem)
-            {
-                case ItemNumber.Bed:
-                    bedModel.SetActive(false);
-                    break;
-                default:
-                    break;
-                
-            }
+        if (curModel){
+            Debug.Log(curModel.name);
         }
-                
+        if (inventoryItemSelected)
+        {
+            //follow cursor 
+            curModel.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        } else //release
+        {
+            inventoryItemSelected = false;
+        }
+     
+    }
+
+    Vector3 ScreenPointToRealPosition(Vector3 screenpos, int distance)
+    {
+        if (Camera.main)
+        {
+            Ray ray = Camera.current.ScreenPointToRay(screenpos);
+            Debug.Log("Camera Position : " + Camera.current.transform.position + "Ray origin: " + ray.origin);
+            return ray.origin + ray.direction * distance;
+        } else
+            return screenpos;
+           
     }
 
     void OnGUI()
@@ -74,22 +69,32 @@ public class InventoryController : MonoBehaviour
         int itemBoxHeight = guiContainerHeight - 20;
 
         GUI.BeginGroup(new Rect(guiContainerLeftMargin, guiContainerTopMargin, guiContainerWidth, guiContainerHeight));
-                
-
+        
         int itemBoxLeftMargin = 10;
         int itemBoxTopMargin = 10;
 
-        if (GUI.Button(new Rect(itemBoxLeftMargin, itemBoxTopMargin, itemBoxWidth, itemBoxHeight), new GUIContent(bedImage), itemBoxStyle))
-        {
-            Debug.Log("Bed clicked");
-            curItem = ItemNumber.Bed;
-        }
-        GUI.Button(new Rect(itemBoxLeftMargin * 2 + itemBoxWidth, itemBoxTopMargin, itemBoxWidth, itemBoxHeight), "item2");
-        GUI.Button(new Rect(itemBoxLeftMargin * 3 + itemBoxWidth * 2, itemBoxTopMargin, itemBoxWidth, itemBoxHeight), "item3");
-        GUI.Button(new Rect(itemBoxLeftMargin * 4 + itemBoxWidth * 3, itemBoxTopMargin, itemBoxWidth, itemBoxHeight), "item4");
-        GUI.Button(new Rect(itemBoxLeftMargin * 5 + itemBoxWidth * 4, itemBoxTopMargin, itemBoxWidth, itemBoxHeight), "item5");
+        Rect button1Box = new Rect(itemBoxLeftMargin, itemBoxTopMargin, itemBoxWidth, itemBoxHeight);
+        Rect button2Box = new Rect(itemBoxLeftMargin * 2 + itemBoxWidth, itemBoxTopMargin, itemBoxWidth, itemBoxHeight);
+        Rect button3Box = new Rect(itemBoxLeftMargin * 3 + itemBoxWidth * 2, itemBoxTopMargin, itemBoxWidth, itemBoxHeight);
+        Rect button4Box = new Rect(itemBoxLeftMargin * 4 + itemBoxWidth * 3, itemBoxTopMargin, itemBoxWidth, itemBoxHeight);
+        Rect button5Box = new Rect(itemBoxLeftMargin * 5 + itemBoxWidth * 4, itemBoxTopMargin, itemBoxWidth, itemBoxHeight);
 
+        GUI.Box(button1Box, new GUIContent(images [0]), itemBoxStyle);
+        GUI.Box(button2Box, "item2", itemBoxStyle);
+        GUI.Box(button3Box, "item3", itemBoxStyle);
+        GUI.Box(button4Box, "item4", itemBoxStyle);
+        GUI.Box(button5Box, "item5", itemBoxStyle);
+
+ 
         GUI.EndGroup();
+
+        Event curEvent = Event.current;
+        Debug.Log(curEvent);
+
+        if(curEvent.type == EventType.mouseDown && button1Box.Contains(Input.mousePosition)){
+            inventoryItemSelected = true;
+            curModel = (GameObject)Instantiate(models[0]);
+        }
     }
 
     private Texture2D plainColor2DTexture(int width, int height, Color color)
@@ -104,4 +109,15 @@ public class InventoryController : MonoBehaviour
         result.Apply(); 
         return result;
     }
+
+    private void DestroyInventoryItemInstances()
+    {
+        foreach (GameObject obj in objectInstanceList)
+        {
+            Destroy(obj);
+        }
+
+        objectInstanceList.Clear();
+    }
+
 }

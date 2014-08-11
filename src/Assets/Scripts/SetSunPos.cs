977ac8f0adc4b1c25a6d049a57e7207079b2a6c7
Moon Sung phill <sp9103@naver.com>
2014-08-11 19:49:48 +0900

    - 태양 터치 초기 위치 설정

--- src/Assets/Scripts/SetSunPos.cs
+++ src/Assets/Scripts/SetSunPos.cs
@@ -7,11 +7,13 @@ public class SetSunPos : MonoBehaviour
 		public GameObject SunLight;					//Control sun light direction.
 		public Camera MainCamera;
 		private Vector3 StructureCenterPos;			//Tracking Target Center
-		private Vector3 TouchPos;
+		private Vector3 CenterToTouch;
 		private bool MouseDownCheck = false;
-		private float RevolutionRadius = 100.0f;	//공전 반지름
-		private int RevolutionFrame = 2000;			//공전 주기(frame)
-		private int FrameCount;						//frame store
+		private float RevolutionRadius = 500.0f;	//공전 반지름
+		private float RevolutionVelocity = 1.0f;		//각 속도
+		private float PresentAngle;					//터치 때는 시점의 각도
+		private float AlphaAngle;
+		private GameObject ImgTarget;
 
 		// Use this for initialization
 		void Start ()
@@ -26,19 +28,26 @@ public class SetSunPos : MonoBehaviour
 
 						if (Input.GetMouseButtonDown (0)) {
 								MouseDownCheck = true;
-								GameObject ImgTarget = GameObject.Find ((ContentManager.getInstance ().imageTargetName));
+								ImgTarget = GameObject.Find ((ContentManager.getInstance ().imageTargetName));
 								this.transform.parent = ImgTarget.transform.GetChild (0);
 								StructureCenterPos = ImgTarget.transform.GetChild (0).position;
-								FrameCount = 0;
 						} else if (Input.GetMouseButtonUp (0)) {
 								MouseDownCheck = false;
 						}
 						
 						//Follow finger
 						if (MouseDownCheck == true) {
-								TouchPos = CalculatePlanePos (Input.mousePosition);
-								this.transform.position = new Vector3 (TouchPos.x, TouchPos.y + RevolutionRadius, TouchPos.z);
+								Vector3 TouchPos = CalculatePlanePos (Input.mousePosition);
+								CenterToTouch = TouchPos - ImgTarget.transform.position;
+								if (CenterToTouch.magnitude / RevolutionRadius > 1.0f)
+										PresentAngle = 0.0f;
+								else
+										PresentAngle = Mathf.Acos (CenterToTouch.magnitude / RevolutionRadius);
+								PresentAngle = 180 * PresentAngle / Mathf.PI;
 
+								AlphaAngle = Vector3.Angle (CenterToTouch, Vector3.left);
+
+								this.transform.position = CalculateSunPos (PresentAngle, CenterToTouch);
 								//Change Light Direction
 								RotateSunLight ();
 						} else {
@@ -48,6 +57,18 @@ public class SetSunPos : MonoBehaviour
 						}
 
 				}
+		}
+
+		private Vector3 CalculateSunPos (float angle, Vector3 Direction)
+		{
+				Vector3 t_SunPos = new Vector3 ();
+				t_SunPos.y = RevolutionRadius * Mathf.Sin (angle);
+
+				float temp = RevolutionRadius * Mathf.Cos (angle);
+				t_SunPos.x = temp * Mathf.Cos (AlphaAngle);
+				t_SunPos.x = temp * Mathf.Sin (AlphaAngle);
+
+				return t_SunPos;
 		}
 
 		private Vector3 CalculatePlanePos (Vector3 MousePos)
@@ -62,12 +83,5 @@ public class SetSunPos : MonoBehaviour
 				Vector3 TargetDir = StructureCenterPos - this.transform.position;
 				// local z-axis <= TargetDir
 				SunLight.transform.forward = TargetDir.normalized;
-		}
-
-		private Vector3 CalculateSunPos ()
-		{
-				FrameCount = (FrameCount + 1) % RevolutionFrame;
-
-				return new Vector3 ();
 		}
 }

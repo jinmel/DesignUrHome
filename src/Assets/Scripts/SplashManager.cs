42a54963cb0c1af8017ab81cf873bb9620d599c1
Jin Suk Park <totorovirus@gmail.com>
2014-08-20 12:47:28 +0900

    Splash Scene added

---
+++ src/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+
+    private Texture splashImage;
+
+	// Use this for initialization
+	void Start () {
+        //draw texture
+        splashImage = Resources.Load<Texture>("splash");
+        StartCoroutine(LoadNextScene(2.0F));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI(){
+        GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),splashImage);
+    }
+
+    IEnumerator LoadNextScene(float delay){
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel("MainScene");
+
+    }
+}

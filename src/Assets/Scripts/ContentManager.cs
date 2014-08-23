1f6891f5290d0786d806ebc0eb8b85855e5df92b
Jin Suk Park <totorovirus@gmail.com>
2014-08-23 22:27:52 +0900

    ContentManager: getInstance method now initializes the instance static variable.

--- src/Assets/Scripts/ContentManager.cs
+++ src/Assets/Scripts/ContentManager.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class ContentManager
 {
-		private static ContentManager instance = new ContentManager ();
+    private static ContentManager instance = null;
 
-		public enum MODE {DEFAULT_MODE, LIGHT_MODE, FURNITURE_MODE, CHARACTER_MODE, RENDER_ONOFF_MODE};
+    public enum MODE
+    {
+        DEFAULT_MODE,
+        LIGHT_MODE,
+        FURNITURE_MODE,
+        CHARACTER_MODE,
+        RENDER_ONOFF_MODE}
+    ;
 
-		public MODE Mode = MODE.DEFAULT_MODE; 
-		public int Flag = 0; // it seperate status of each mode.
-		public string imageTargetName;	//present tracking ImageTarget name.
-		public Rect UI_Domain;
+    public MODE Mode = MODE.DEFAULT_MODE;
+    public int Flag = 0; // it seperate status of each mode.
+    public string imageTargetName;  //present tracking ImageTarget name.
+    public Rect UI_Domain;
 
-		public static ContentManager getInstance ()
-		{
-				return instance;
-		}
+    public static ContentManager getInstance()
+    {
+        if(instance == null)
+            instance = new ContentManager();
+        return instance;
+    }
 }
